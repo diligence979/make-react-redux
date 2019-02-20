fa4abc5961212e546ab7501f21b1c79a37894e45
sophia <sophia_yangshuyu@163.com>
2019-02-20 15:42:25 +0800

    commit all file

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from './react-redux';
+
+class Header extends Component {
+    static propTypes = {
+        themeColor: PropTypes.string
+    }
+
+    constructor () {
+        super()
+        this.state = { themeColor: '' }
+    }
+
+    render () {
+        return (
+            <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+Header = connect(mapStateToProps)(Header)
+
+export default Header
